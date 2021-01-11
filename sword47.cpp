ef9ae94029d453d735000b9b32d3dfce27d8a0fb
Tommy9609 <60131292+Tommy9609@users.noreply.github.com>
2021-01-11 20:40:15 +0800

    Update sword47.cpp

--- sword47.cpp
+++ sword47.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int maxValue(vector<vector<int>>& grid) {
         int m=grid.size();       //行
@@ -16,5 +16,28 @@ public:
             ans[i][j]=max(ans[i-1][j],ans[i][j-1])+grid[i][j];
         }
         return ans[m-1][n-1];
+    }
+};
+
+class Solution2 {
+public:
+    int maxValue(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();    //列
+        vector<int> ans(n,0);
+        ans[0]=grid[0][0];
+        for(int i=1;i<n;i++){
+            ans[i]=ans[i-1]+grid[0][i];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(j==0)
+                  ans[0]=ans[0]+grid[i][0];
+                else{
+                  ans[j]=max(ans[j-1],ans[j])+grid[i][j];
+                }
+            }
+        }
+        return ans[n-1];
     }
 };
